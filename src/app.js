a7468cb8cc48beaade8724c73729df6fca9a153a
GuillermoCairo26 <guillermocairo14@gmail.com>
2021-04-22 14:08:45 +0000

    Primera parte ejercicio. Parte visual sin JS

--- src/app.js
+++ src/app.js
@@ -6,6 +6,20 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let figura = ["♥", "♦", "♠", "♣"];
+  let numero = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
 };

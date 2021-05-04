fba1a12ef5a144ab0ddf8a9545b95d5ebd2cf2ec
GuillermoCairo26 <guillermocairo14@gmail.com>
2021-05-04 13:23:41 +0000

    Finalizado

--- src/app.js
+++ src/app.js
@@ -2,9 +2,6 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
   let figura = ["♥", "♦", "♠", "♣"];
   let numero = [
@@ -22,4 +19,24 @@ window.onload = function() {
     "Q",
     "K"
   ];
+
+  //   Crea variable con figura random
+  let figuraRandom = figura[Math.floor(Math.random() * figura.length)];
+  //   Crea variable con número random
+  let numeroRandom = numero[Math.floor(Math.random() * numero.length)];
+
+  document.querySelector("#figura1").innerHTML = `${figuraRandom}`;
+  document.querySelector("#numero").innerHTML = `${numeroRandom}`;
+  document.querySelector("#figura2").innerHTML = `${figuraRandom}`;
+
+  //   Si la figura random es corazones o diamantes se pinta rojo
+  if (figuraRandom == "♥" || figuraRandom == "♦") {
+    figura1.style.color = "red";
+    figura2.style.color = "red";
+  }
+  //  Si la figura random es picas o tréboles
+  if (figuraRandom == "♠" || figuraRandom == "♣") {
+    figura1.style.color = "black";
+    figura2.style.color = "black";
+  }
 };
